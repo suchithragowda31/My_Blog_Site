7b475c96a3cf55e04b4cea98f4e18325ef50a871
suchithragowda31 <suchithragowdadpg@gmail.com>
2025-07-25 13:31:01 +0530

    Add basename for GitHub Pages routing

--- navigation/AppRouter.jsx
+++ navigation/AppRouter.jsx
@@ -1,13 +1,6 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../pages/Home";
-// import Navbar from '../components/Navbar';
-// import AllBlogs from '../pages/AllBlogs';
-// import CreateBlog from '../pages/CreateBlog';
-// import BlogDetail from '../pages/BlogDetail';
-
-
-
 
 const router = createBrowserRouter([
     // Public Routes
@@ -15,16 +8,10 @@ const router = createBrowserRouter([
         path: '/',
         element: <Home />,
     },
-    // {   
-    //     path: '/gallery',
-    //     element: <GallerySection />,
-    // },
-    // {  
-    //     path: '/contact',
-    //     element: <ContactSection />,
-    // }
    
-]);
+], {
+    basename: '/My_Blog_Site'  // Add this line!
+});
 
 const AppRouter = () => {
     return <RouterProvider router={router} />;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: '/My_Blog_Site/',
   plugins: [
+    react(),
     tailwindcss(),
   ],
 })
